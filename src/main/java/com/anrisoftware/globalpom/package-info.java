a351186ce8be9b177b74f7d66bc7120a0c6a6397
Erwin Mueller <erwin.mueller@deventm.org>
2014-06-04 22:05:42 +0200

    Add package-info.

---
+++ src/main/java/com/anrisoftware/globalpom/package-info.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2011-2014 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of globalpom-log.
+ *
+ * globalpom-log is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * globalpom-log is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with globalpom-log. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * <h1>Global POM Logging</h1>
+ *
+ * <p>
+ * Logger factory to inject a logger for a class.
+ *
+ * <h2>Maven Module</h2>
+ *
+ * <pre>
+ * &lt;dependency>
+ * &lt;groupId>com.anrisoftware.globalpom&lt;/groupId>
+ * &lt;artifactId>globalpomutils-log&lt;/artifactId>
+ * &lt;version>1.16&lt;/version>
+ * &lt;/dependency>
+ * </pre>
+ *
+ * <h2>GitHub SCM</h2>
+ *
+ * <ul>
+ * <li><a href="https://github.com/devent/globalpom-log.git">Repository</a></li>
+ * <li><a href="git://git@github.com:devent/globalpom-log.git">Repository SSH</a>
+ * <li><a href="https://github.com/devent/globalpom-log.git">Repository HTTPS</a>
+ * </ul>
+ *
+ * @version 1.16
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ */
+package com.anrisoftware.globalpom;
+
